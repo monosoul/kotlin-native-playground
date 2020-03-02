89077395e76e32a8c8427d7df29f79800559f494
monosoul <andrei.nevedomskii@n26.com>
2020-03-02 15:01:52 +0100

    Configure common source set and build targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,44 @@
 group = "com.github.monosoul"
 version = "0.0.1"
+
+plugins {
+    kotlin("multiplatform") version "1.3.61"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+
+    linuxX64("linux") {
+        binaries {
+            executable()
+        }
+    }
+
+    mingwX64("windows") {
+        binaries {
+            executable()
+        }
+    }
+    
+    macosX64("macos") {
+        binaries {
+            executable()
+        }
+    }
+}
